edd12f0fe3d6343c542a1a79abed88173bccbf6d
Joshua Fernandes <joshferns0212@gmail.com>
2019-05-23 09:06:17 -0400

    Modified lab1_polynomial

--- lab1_polynomial.hpp
+++ lab1_polynomial.hpp
@@ -1,3 +1,8 @@
+//Joshua Fernandes 20765616
+//Eric-Khang Dao 2074442
+#ifndef LAB1_POLYNOMIAL_H
+#define LAB1_POLYNOMIAL_H
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -5,17 +10,31 @@
 using namespace std;
 
 class Polynomial {
-    vector<int> poly_data;
-
+	vector<int> poly_data;
 public:
-
-    Polynomial(int A[], int size);
-    Polynomial();
-    Polynomial(string fileName);
-    bool operator==(const Polynomial& target);
-    void print();
-    Polynomial operator+(const Polynomial& target);
-    Polynomial operator-(const Polynomial& target);
-    Polynomial operator*(const Polynomial& target);
-    Polynomial derivative();
+	Polynomial(int A[], int size);
+	Polynomial();
+	Polynomial(string fileName);
+	bool operator==(const Polynomial& target);
+	void print();
+	Polynomial operator+(const Polynomial& target);
+	Polynomial operator-(const Polynomial& target);
+	Polynomial operator*(const Polynomial& target);
+	Polynomial derivative();
+	friend class PolynomialTest;
 };
+
+class PolynomialTest {
+public:
+	bool test_parameterized_constructor();
+	bool test_default_constructor();
+	bool test_file_constructor();
+	bool test_equals();
+	bool test_plus();
+	bool test_minus();
+	bool test_mult();
+	bool test_derivative();
+	void test_print();
+	void run();
+};
+#endif // LAB1_POLYNOMIAL_H
